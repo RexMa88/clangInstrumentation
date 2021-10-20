ae98a2710bbde4208ce559d537ff0299af259c23
machao13 <machao13@longfor.com>
2021-10-20 10:46:32 +0800

    Initial Commit

---
+++ clangInsertPile/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  clangInsertPile
+//
+//  Created by machao on 2021/10/20.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
